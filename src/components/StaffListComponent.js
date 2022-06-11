a2675c9611d0baa4eac465c0c700dd56feaefe79
Nguyen Ngoc Nhan <nhannnfx16071@funix.edu.vn>
2022-06-11 21:01:09 +0700

    commit 1

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,1 +1,12 @@
+import React, { useState } from "react";
+import { STAFFS } from "../shared/constants";
 
+//Declaring StaffList
+const StaffList = () => {
+  const [staffs, setStaffs] = useState(STAFFS);
+  const [selectedStaff, setSelectedStaff] = useState(null);
+  return (
+    <div className="container">
+    </div>)
+}
+export default StaffList;
