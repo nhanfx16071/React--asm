01b34892fca3c35d4795dda943ab97280e4332d7
Nguyen Ngoc Nhan <nhannnfx16071@funix.edu.vn>
2022-06-12 18:05:14 +0700

    commit 10

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import { STAFFS } from "../shared/constants";
+import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
+import dateFormat from "dateformat";
+
 //Declaring StaffList
 const StaffList = () => {
   const [staffs, setStaffs] = useState(STAFFS);
   const [selectedStaff, setSelectedStaff] = useState(null);
+  const [columDefault, setColumDefault] = useState(
+    "col-12 col-md-6 col-lg-4 mt-3"
+  );
+  //Declaring columnSelect
+  const columnSelect = (col) => {
+    setColumDefault(col);
+  };
   //Declaring Staff
   const Staff = () => {
     if (selectedStaff !== null) {
@@ -25,10 +35,6 @@ const StaffList = () => {
                 selectedStaff.startDate,
                 "dd/mm/yyyy"
               )}`}</CardText>
-              <CardText>{`Ngày vào công ty: ${dateFormat(
-                selectedStaff.startDate,
-                "dd/mm/yyyy"
-              )}`}</CardText>
               <CardText>{`Phòng ban: ${selectedStaff.department.name}`}</CardText>
               <CardText>{`Số ngày nghỉ còn lại: ${selectedStaff.annualLeave}`}</CardText>
               <CardText>{`Số ngày đã làm thêm: ${selectedStaff.overTime}`}</CardText>
@@ -40,6 +46,7 @@ const StaffList = () => {
       return <div></div>;
     }
   };
+  // Set up button to change quantity of columms
   return (
     <div className="container">
       <div
@@ -91,5 +98,18 @@ const StaffList = () => {
           justifyContent: "center",
         }}
       >
-}
-        export default StaffList;
+        {staffs.map((staff) => (
+          <Card
+            className={columDefault}
+            onClick={() => setSelectedStaff(staff)}
+          >
+            <CardTitle>{staff.name}</CardTitle>
+          </Card>
+        ))}
+      </div>
+      <Staff />
+    </div>
+  );
+};
+
+export default StaffList;
